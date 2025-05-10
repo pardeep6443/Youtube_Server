587a0945b83fe2078307d7fd65076177e8d3a676
pardeep6443 <116626777+pardeep6443@users.noreply.github.com>
2025-05-10 13:24:34 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { update, deleteUser, subscribe, unsubscribe, like, dislike, getUser } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
+const router = express.Router();
+
+//update user
+router.put("/:id", verifyToken, update)
+//delete user
+router.delete("/:id", verifyToken, deleteUser)
+//get a user
+router.get("/find/:id", getUser)
+//subscribe a user
+router.put("/sub/:id", verifyToken, subscribe)
+//unsubscribe a user
+router.put("/unsub/:id", verifyToken, unsubscribe)
+//like a video
+router.put("/like/:videoId", verifyToken, like)
+//dislike a video
+router.put("/dislike/:videoId", verifyToken, dislike)
+
+export default router;
